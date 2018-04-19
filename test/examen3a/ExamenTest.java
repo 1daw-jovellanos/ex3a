509b08a4574133fe49558a40cb631f86d985e40a
Víctor J. Fernández <victor.fernandezgil@educa.madrid.org>
2018-04-19 10:26:15 +0200

    Completa

--- test/examen3a/ExamenTest.java
+++ test/examen3a/ExamenTest.java
@@ -15,19 +15,17 @@ public class ExamenTest {
     static double nota;
     Ficus f1, f2;
     Maceta m1;
-    
+
     static {
         nota = 0;
     }
-    
+
     /**
      * Default constructor for test class ExamenTest
      */
     public ExamenTest() {
     }
 
-    
-    
     /**
      * Sets up the test fixture.
      *
@@ -35,9 +33,9 @@ public class ExamenTest {
      */
     @Before
     public void setUp() {
-         f1 = new Ficus(100);
-         f2 = new Ficus(200);
-         m1 = new Maceta(100 * 0.15, 10);
+        f1 = new Ficus(100);
+        f2 = new Ficus(200);
+        m1 = new Maceta(100 * 0.15, 10);
     }
 
     /**
@@ -53,11 +51,12 @@ public class ExamenTest {
     public static void before() {
         nota = 0;
     }
+
     @AfterClass
     public static void after() {
         System.out.format("TOTAL: %.2f%n", nota);
     }
-    
+
     @Test
     public void Ejercicio1() {
         double notaLocal = 0;
@@ -77,24 +76,20 @@ public class ExamenTest {
         String producida = examen1.listarTodo();
         double dleve = levenshtein(producida, esperado);
 
-        
-
         assertEquals("La cadena producida es completamente distinta a lo esperado",
                 dleve >= 100, false);
-        
 
         assertEquals("La cadena producida es más de un 50% distinta a lo esperado",
                 dleve > 50, false);
-        
 
         assertEquals("La cadena producida es más de un 25% distinta a lo esperado",
                 dleve > 25, false);
-        
+
         // Puntua a partir de que el resultado falla en menos de un 25%
-        notaLocal = 2.5 * ((100.0 - dleve)/ 100);
+        notaLocal = 2.5 * ((100.0 - dleve) / 100);
         System.out.format("Ejercicio 1: %.2f%n", notaLocal);
         nota += notaLocal;
-        
+
         assertEquals("La cadena producida es más de un 10% distinta a lo esperado",
                 dleve > 10, false);
         assertEquals("La cadena producida es más de un 5% distinta a lo esperado",
@@ -121,17 +116,20 @@ public class ExamenTest {
         l.add(new Jarron(10000.1, 3));
         l.add(new Maceta(10, 300));
         assertEquals("Hay  un producto de más de 10000 en la lista se espera que el metodo devuelva true", true, examen1.hayProductoDeMasDe10000());
-        for (int i = 0; i < 10000000;i++) {
+
+        // Con tiempo, para comprobar que se hace un recorrido parcial.
+        for (int i = 0; i < 15000000; i++) {
             l.add(new Maceta(11000, 200));
         }
         Date bef = new Date();
+        assertEquals("Hay  un producto de más de 10000 en la lista se espera que el metodo devuelva true", true, examen1.hayProductoDeMasDe10000());
         Date aft = new Date();
-        
-        long dif = aft.getTime()-bef.getTime();
 
-        assertEquals("El algoritmo parece muy lento.", true, dif < 100);
-        
-        
+        long dif = aft.getTime() - bef.getTime();
+
+        System.out.format("Ejercicio 2: Tiempo de ejecución para 15000000 de objetos:%dms%n", dif);
+        assertEquals("El algoritmo parece muy lento. ¿Tarda menos de 20ms en retornar para un caso grande?", true, dif < 20);
+
         notaLocal = 1.0;
         System.out.format("Ejercicio 2: %.2f%n", notaLocal);
         nota += notaLocal;
@@ -140,36 +138,39 @@ public class ExamenTest {
     @Test
     public void Ejercicio3a() {
         double notaLocal = 0;
-
+        assertEquals("No deben añadirse variables de instancia al ficus. Está explícitamente indicado en el enunciado", true, f1.getClass().getDeclaredFields().length == 0);
         assertEquals("El descuento esperado no coincide ", 10.0, f1.getDescuento(), 0.001);
         assertEquals("El descuento esperado no coincide ", 10.0, f2.getDescuento(), 0.001);
         notaLocal = 0.5;
         System.out.format("Ejercicio 3 (getDescuento): %.2f%n", notaLocal);
         nota += notaLocal;
-  
+
     }
-    
-     @Test
+
+    @Test
     public void Ejercicio3b() {
         double notaLocal = 0;
+        assertEquals("No deben añadirse variables de instancia al ficus. Está explícitamente indicado en el enunciado", true, f1.getClass().getDeclaredFields().length == 0);
 
         assertEquals("El precio esperado no coincide ", 100 * 0.15 * 0.9, f1.getPrecioMes(), 0.001);
         assertEquals("El precio esperado no coincide ", 200 * 0.15 * 0.9, f2.getPrecioMes(), 0.001);
-       notaLocal = 0.5;
+        notaLocal = 0.5;
         System.out.format("Ejercicio 3 (getPrecioMes): %.2f%n", notaLocal);
         nota += notaLocal;
     }
-    
-     @Test
+
+    @Test
     public void Ejercicio3c() {
         double notaLocal = 0;
-
+        assertEquals("No deben añadirse variables de instancia al ficus. Está explícitamente indicado en el enunciado", true, f1.getClass().getDeclaredFields().length == 0);
         assertEquals("Una maceta y un ficus no pueden ser iguales", false, f1.equals(m1));
-        assertEquals("Dos ficus con distinta altura no pueden ser iguales", false, f1.equals(f2));
+        assertEquals("Dos ficus con  alturas muy distintas no pueden ser iguales", false, f1.equals(f2));
         assertEquals("Un ficus debe ser igual a sí mismo", true, f1.equals(f1));
         assertEquals("Un ficus debe ser igual a otro ficus de la misma altura", true, f1.equals(new Ficus(100)));
+        assertEquals("Consideramos un ficus igual a otro si su precio difiere en menos de 20 centimos", true, f1.equals(new Ficus(99)));
+
         notaLocal = 0.5;
-        System.out.format("Ejercicio 3 (equals): %.2f%n", notaLocal);
+        System.out.format("Ejercicio 3 (equals.): %.2f%n", notaLocal);
         nota += notaLocal;
     }
 
